d956c5d03605d11d8e8fd3f5c2559cdbe77c7856
antivoland <antivoland@inbox.ru>
2014-03-14 00:37:57 +0400

    let's start with incall-service

---
+++ src/main/java/system/incall/Main.java
@@ -0,0 +1,12 @@
+package system.incall;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("It's alive!");
+    }
+}
