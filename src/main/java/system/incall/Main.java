9b9f4835b106016d70be4ce0a35d9ce8596b6a17
antivoland <antivoland@inbox.ru>
2014-03-20 00:21:16 +0400

    mongo params

--- src/main/java/system/incall/Main.java
+++ src/main/java/system/incall/Main.java
@@ -2,8 +2,10 @@ package system.incall;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
+import org.freeswitch.esl.client.IEslEventListener;
 import org.freeswitch.esl.client.inbound.Client;
 import org.freeswitch.esl.client.inbound.InboundConnectionFailure;
+import org.freeswitch.esl.client.transport.event.EslEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,6 +31,35 @@ public class Main {
                 } catch (InboundConnectionFailure e) {
                     throw new Error(e);
                 }
+
+                client.setEventSubscriptions("plain", "all");
+
+                /*
+                client.addEventFilter("Event-Name", "CHANNEL_CREATE");
+                client.addEventFilter("Event-Name","BACKGROUND_JOB");
+                client.addEventFilter("Event-Name","CHANNEL_STATE");
+                client.addEventFilter("Event-Name","CHANNEL_EXECUTE_COMPLETE");
+                client.addEventFilter("Event-Name","CHANNEL_HANGUP");
+                client.addEventFilter("Event-Name","CHANNEL_HANGUP_COMPLETE");
+                client.addEventFilter("Event-Name","DTMF");
+                client.addEventFilter("Event-Name","HEARTBEAT");
+                */
+
+                client.addEventListener(new IEslEventListener() {
+                    @Override
+                    public void eventReceived(EslEvent event) {
+                        log.info( "Event received [{}]", event );
+                        log.info( "==========================================================================" );
+                    }
+
+                    @Override
+                    public void backgroundJobResultReceived(EslEvent event) {
+                        log.info( "Background job result received [{}]", event );
+                    }
+                });
+
+                client.sendSyncApiCommand("echo", "Foo foo bar");
+
                 bind(Client.class).toInstance(client);
             }
         });
