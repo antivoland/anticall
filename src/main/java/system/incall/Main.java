7d3056b89909f8a5db4c6fc6c4170e2c0eca7936
antivoland <antivoland@inbox.ru>
2014-03-25 23:58:00 +0400

    ma playing with da filez

--- src/main/java/system/incall/Main.java
+++ src/main/java/system/incall/Main.java
@@ -1,17 +1,22 @@
 package system.incall;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.freeswitch.esl.client.IEslEventListener;
 import org.freeswitch.esl.client.inbound.Client;
 import org.freeswitch.esl.client.transport.event.EslEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import system.incall.dao.ProfileDao;
+import system.incall.dao.ProfileDaoMorphiaImpl;
 import system.incall.guice.EslModule;
 import system.incall.guice.MorphiaModule;
 import system.incall.model.Event;
 import system.incall.model.EventConsumer;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class Main {
@@ -20,7 +25,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         App.properties = new Properties();
         App.properties.load(new FileInputStream("conf/incall.properties"));
-        App.injector = Guice.createInjector(new EslModule(), new MorphiaModule());
+        App.injector = Guice.createInjector(new EslModule(), new MorphiaModule(), new AbstractModule() {
+            @Override
+            protected void configure() {
+                bind(ProfileDao.class).to(ProfileDaoMorphiaImpl.class);
+            }
+        });
 
         Client eslClient = App.injector.getInstance(Client.class);
         eslClient.setEventSubscriptions("plain", "all");
@@ -36,6 +46,12 @@ public class Main {
             @Override
             public void eventReceived(EslEvent event) {
                 Event.valueOf(event.getEventName()).consume(event);
+                ObjectMapper mapper = new ObjectMapper();
+                try {
+                    log.debug(mapper.writeValueAsString(event));
+                } catch (IOException e) {
+                    log.warn(e.getMessage());
+                }
                 log.info("================================================================================");
             }
 
---
+++ src/main/java/system/incall/dao/ProfileDao.java
@@ -0,0 +1,8 @@
+package system.incall.dao;
+
+public interface ProfileDao {
+    boolean exists(String userId);
+    Profile get(String userId);
+    void save(Profile profile);
+    void delete(String userId);
+}
